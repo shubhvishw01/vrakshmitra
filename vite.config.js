87e91c2efd26055e114c926ed02dea34a5935b25
Shubham Vishwakarma <shubh.vishw01@gmail.com>
2025-11-04 19:48:07 +0530

    vms version 0.2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(),
     tailwindcss(),
   ],
