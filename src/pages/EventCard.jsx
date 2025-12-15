0da33f3bd388b8945c416d4b69e488a36c4e1f13
Shubham Vishwakarma <shubh.vishw01@gmail.com>
2025-12-15 19:29:34 +0530

    Update moblile & desktop priview section

--- src/pages/EventCard.jsx
+++ src/pages/EventCard.jsx
@@ -81,15 +81,12 @@ const EventCard = React.memo(({ image, place, desc, date }) => {
             /* Mobile */
             onTouchEnd={handleMobileTap}
             className={`
-              max-w-full max-h-full rounded-xl shadow-2xl
-              transition-transform duration-500
-              ${zoom ? "scale-150" : "scale-100"}
-              ${!isMobile && zoom ? "cursor-zoom-out" : ""}
-              ${!isMobile && !zoom ? "cursor-zoom-in" : ""}
-            `}
-            style={{
-              touchAction: "manipulation",
-            }}
+    max-w-full max-h-full rounded-xl shadow-2xl
+    transition-transform duration-500
+    ${zoom ? "scale-150" : "scale-100"}
+    ${!isMobile && zoom ? "cursor-zoom-out" : ""}
+    ${!isMobile && !zoom ? "cursor-zoom-in" : ""}
+  `}
           />
         </div>
       )}
